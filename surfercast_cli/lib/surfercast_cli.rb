b1f374ba1ff86cbd7d06b772d02b5af2f766774e
github username <github email address>
2020-06-16 10:17:55 -0500

    some commit message

---
+++ surfercast_cli/lib/surfercast_cli.rb
@@ -0,0 +1,6 @@
+require "surfercast_cli/version"
+
+module SurfercastCli
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ surfercast_cli/lib/surfercast_cli/version.rb
@@ -0,0 +1,3 @@
+module SurfercastCli
+  VERSION = "0.1.0"
+end
